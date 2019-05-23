8d5e5a0a72942a955fe6158980f3805089934d13
Harry beckwith <harry.beckwith@itech.media>
2019-05-23 14:35:09 +0000

    router path

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom';
 const Links = () => {
   return (
     <React.Fragment>
-
       <NavLink exact activeClassName='menu__nav__active' to="/">Home</NavLink>
       <NavLink activeClassName='menu__nav__active' to="/sites">Sites</NavLink>
       <NavLink activeClassName='menu__nav__active' to="/projects">projects</NavLink>
