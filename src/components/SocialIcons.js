3ed7155e0a08868b48483ac488b566095b54778e
Harry beckwith <harry.beckwith@itech.media>
2019-05-23 09:28:20 +0000

    test image path

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -3,15 +3,18 @@ import React from 'react';
 const SocialIcons = () => {
   return (
     <React.Fragment>
+
+      aass
       <a
         href="https://www.instagram.com/harry_j_beckwith/?hl=en"
         target="_blank"
       >
         <img
           className="menu__icons__icon"
-          src="/icons/instagram-logo.svg"
+          src="./images/icons/instagram-logo.svg"
           alt="instagram logo"
         />
+
       </a>
       <a
         href="https://github.com/harrybeckwith"
