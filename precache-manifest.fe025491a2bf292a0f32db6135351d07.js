8cc6417b04ada73519abe66bcca6c0adf3853b25
Harry beckwith <harry.beckwith@itech.media>
2019-05-23 10:25:35 +0000

    Updates

---
+++ precache-manifest.fe025491a2bf292a0f32db6135351d07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af53612f32b78455d832c237b4c54ed",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "2633c2d1f5011a168417",
+    "url": "/port/static/css/main.e2dc52ad.chunk.css"
+  },
+  {
+    "revision": "dba5d4d9cd27477b14ac",
+    "url": "/port/static/js/2.5e7d1381.chunk.js"
+  },
+  {
+    "revision": "2633c2d1f5011a168417",
+    "url": "/port/static/js/main.72a3c19a.chunk.js"
+  },
+  {
+    "revision": "2a7ae5c5c38d7d23e3c1",
+    "url": "/port/static/js/runtime~main.a7676607.js"
+  }
+]);
